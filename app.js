09bb479b407e1fe6710174348a176bf72ac60524
tchovis20 <enzocueio123@gmail.com>
2026-02-27 08:36:01 -0300

    calculadora atualizada

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Eu sabo")
+const numero1 = document.getElementById("nota1")
+const numero2= document.getElementById("nota2")
+const button= document.getElementById("button")
+
+function calculaMedia(){
+    const nota1 = Number(numero1.value)
+    const nota2 = Number(numero2.value)
+
+    const media = (nota1 + nota2)/2
+
+    console.log(media)
+}
+
+button.addEventListener("click", calculaMedia)
