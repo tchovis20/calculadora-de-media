60e221e4e6bd9b7b5d073e0124130dabe3cfce33
tchovis20 <enzocueio123@gmail.com>
2026-03-02 10:17:39 -0300

    calculadora tchovis

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const numero1 = document.getElementById("nota1")
 const numero2= document.getElementById("nota2")
 const button= document.getElementById("button")
+const resultado= document.getElementById("resultado")
 
 function calculaMedia(){
     const nota1 = Number(numero1.value)
@@ -8,7 +9,7 @@ function calculaMedia(){
 
     const media = (nota1 + nota2)/2
 
-    console.log(media)
+    resultado.innerHTML = `A média é ${media}`
 }
 
 button.addEventListener("click", calculaMedia)
